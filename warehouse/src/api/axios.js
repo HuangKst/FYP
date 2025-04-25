3e550312d92bd160ee6f3c1600bed1d1af5d00c9
HuangKst <1300105758@qq.com>
2025-04-25 23:22:57 +0100

    tigger vercel

--- warehouse/src/api/axios.js
+++ warehouse/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
     baseURL: process.env.REACT_APP_API_BASE_URL,
 });
- 
+
 instance.interceptors.request.use(
     (config) => {
         const token = localStorage.getItem('token');
